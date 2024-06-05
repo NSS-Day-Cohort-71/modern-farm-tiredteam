856ae23b16a7847c1d36ea0961608f256a8166c4
Isabel Aparicio <iaparicio695@gmail.com>
2024-06-05 15:45:54 -0400

    Merged main branch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,12 @@ import { createPotato } from './seeds/potato.js'
 import { createSoybean } from './seeds/soybean.js'
 import { createSunflower } from './seeds/sunflower.js'
 import { createWheat } from './seeds/wheat.js'
+import { addPlant } from "./field.js"
+import { usePlants } from "./field.js";
 
 console.log("Welcome to the main module")
 
 const yearlyPlan = createPlan()
-// console.log(yearlyPlan)
 const asparagusSeed = createAsparagus()
 console.log(asparagusSeed)
 const cornSeed = createCorn()
