04e63be297923c194c0118c06818a068c54701a2
Lane Dillard <lane.holland04@gmail.com>
2024-06-05 16:00:37 -0500

    More fixed conflicts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,9 @@
 import { createPlan } from './plan.js'
-import { createAsparagus } from './seeds/asparagus.js'
-import { createCorn } from './seeds/corn.js'
-import { createPotato } from './seeds/potato.js'
-import { createSoybean } from './seeds/soybean.js'
-import { createSunflower } from './seeds/sunflower.js'
-import { createWheat } from './seeds/wheat.js'
 import { addPlant } from "./field.js"
 import { usePlants } from "./field.js";
-
 import { plantSeeds } from './tractor.js'
 
-console.log("Welcome to the main module")
+
 
 const yearlyPlan = createPlan()
 
