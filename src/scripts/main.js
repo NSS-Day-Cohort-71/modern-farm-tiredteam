f746da9fcdd5d51efc4023d489ec7aee65e7b9f5
Isabel Aparicio <iaparicio695@gmail.com>
2024-06-06 12:06:34 -0400

    Finished CSS styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,16 @@
 import { createPlan } from './plan.js'
 import { plantSeeds } from './tractor.js'
 import { harvestPlants } from './harvester.js'
+import { catalog } from './catalog.js'
 
 const yearlyPlan = createPlan()
 const growingPlants = plantSeeds(yearlyPlan)
 const harvestedPlants = harvestPlants(growingPlants)
-console.log(harvestedPlants)
+const HTML = catalog(harvestedPlants)
+
+const renderPlantsToDOM = document.querySelector(".container")
+
+renderPlantsToDOM.innerHTML = HTML;
 
 
 
